4d66d56cde67570dc7a7b312bb808e8cc4310de8
Ashish Shrestha <aashishshrestha5532@gmail.com>
2023-01-11 13:19:59 +0545

    decrease border line

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="border-t-2 border-gray-500 p-5 text-center w-full fixed bottom-0">
+    <footer className="border-t-2 border-gray-600 p-5 text-center w-full fixed bottom-0">
       <p className="font-bold">Neptune Mutual &#169; 2023</p>
     </footer>
   );
