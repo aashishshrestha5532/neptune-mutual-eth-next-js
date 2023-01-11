fe5628329cc1e58756fffc71241c0d2be9e6997f
Ashish Shrestha <aashishshrestha5532@gmail.com>
2023-01-11 08:44:45 +0545

    wrap in web3 provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import Web3 from "web3";
+import { provider } from "web3-core";
+import { Web3ReactProvider } from "@web3-react/core";
+
+function getLibrary(provider: provider) {
+  return new Web3(provider);
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  );
 }
